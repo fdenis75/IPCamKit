ec2911d8610090a698760b164d7b4185e67758ad
fdenis75 <Francois.denis@gmail.com>
2026-03-14 17:59:15 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,8 +5,7 @@ import PackageDescription
 let package = Package(
   name: "IPCamKit",
   platforms: [
-    .macOS(.v14)
-  ],
+ platforms: [.macOS(.v15), .iOS(.v15), .macCatalyst(.v15)],  ],
   products: [
     .library(
       name: "IPCamKit",
